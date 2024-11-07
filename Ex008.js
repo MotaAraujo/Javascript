4617f5550f1826329b4b0d7840548ecf6f23bb7d
MotaAraujo <motaaraujo@outlook.com>
2024-11-07 11:24:57 -0300

    Aulas JavaScript

---
+++ Ex008.js
@@ -0,0 +1,6 @@
+var vel = 50
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) { //CONDIÇÃO SIMPLES
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija semre usando cinto de segurança!`)
